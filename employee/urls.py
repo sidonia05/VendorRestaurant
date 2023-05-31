022b1a87cb2331af8b925bcd66e82406cfc1421d
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-31 14:43:25 +0300

    to_do_part1

--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from employee.views import EmployeeView
+from employee.views import EmployeeView, EmployeeDetailsView
 
 urlpatterns = [
     path('employee/', EmployeeView.as_view(), name='employee'),
+    path('employee/details/<int:pk>/', EmployeeDetailsView.as_view(), name='employee_details'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from employee.models import Employee
 
@@ -6,3 +6,8 @@ class EmployeeView(View):
     def get(self, request):
         employees = Employee.objects.all()
         return render(request, 'employee/employee.html', {'employees': employees})
+
+class EmployeeDetailsView(View):
+    def get(self, request, pk):
+        employee = get_object_or_404(Employee, pk=pk)
+        return render(request, 'employee/employees-details.html', {'employee': employee})

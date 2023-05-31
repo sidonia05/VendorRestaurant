25e7a5a44f42d6c1872d4b9a52f6b4c0cca1a6bc
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-31 16:05:21 +0300

    todo_1

--- VendorRestaurant/urls.py
+++ VendorRestaurant/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('', include('employee.urls')),
+    # path('',include('todo.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ todo/views.py
@@ -0,0 +1,11 @@
+# # views.py
+#
+# from django.shortcuts import render
+#
+# def task_create(request):
+#     # Add your logic for task creation here
+#     return render(request, 'todo/task_create.html')
+#
+# def task_list(request):
+#     tasks = []  # Add your logic to fetch the list of tasks here
+#     return render(request, 'todo/task_list.html', {'tasks': tasks})

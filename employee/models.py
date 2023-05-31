3922ab1f715e80a286bc0634935059731949ba32
sidonia05 <sidoniaraluca1994@gmail.com>
2023-05-31 11:17:00 +0300

    table

--- employee/models.py
+++ employee/models.py
@@ -10,8 +10,8 @@ class Employee(models.Model):
     email_address = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=13, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
-        return self.first_name
+        return f'{self.first_name} {self.last_name}'
--- employee/views.py
+++ employee/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.views import View
-
 from employee.models import Employee
-
 
 class EmployeeView(View):
     def get(self, request):
-        # Retrieve all employees from the database
         employees = Employee.objects.all()
-
-        # Pass the employees to the template for rendering
-        return render(request, 'employee/employee.html',
-                      {'employees': employees})
+        return render(request, 'employee/employee.html', {'employees': employees})
